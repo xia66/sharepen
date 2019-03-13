3b4befaf63d1a77b5ba9b9c40caa00d3d3e9b6ac
xiachengyu_i <xiachengyu_i@didichuxing.com>
2019-03-13 19:43:52 +0800

    注释添加

--- examples/server.js
+++ examples/server.js
@@ -6,12 +6,11 @@ var io = require('socket.io')(http)
 // var path = require('path')
 
 http.listen(4000, function () {
-  console.log('listening on *:4000')
+    console.log('listening on *:4000')
 })
-
 var EditorSocketIOServer = require('../build/SharedPenServer.js')
-var server = new EditorSocketIOServer('', [], 1)
+var server = new EditorSocketIOServer('', [], 1)    //修改：点击文档后获取数据库存储的富文本作为第一个参数，文档id作为第三个参数
 
-io.on('connection', function (socket) {
-  server.addClient(socket)
+io.on('connection', function (socket) { //每次连接都会产生一个新socket
+    server.addClient(socket)
 })
